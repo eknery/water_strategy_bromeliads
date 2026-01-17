d962bf982ee415a076d95b4b8460a93b4365c6cb
Eduardo Koerich Nery <91353128+eknery@users.noreply.github.com>
2026-01-17 11:30:33 -0300

    update

---
+++ scripts/downloading_genbank.R
@@ -0,0 +1,33 @@
+if(!require("ape")) install.packages("ape"); library("ape")
+
+### input directory
+dir_input = "0_data/"
+### read acessions table
+access = read.csv(paste0(dir_input, "taxon_access.csv") )
+
+### loci names
+all_loci = colnames(access)[colnames(access) != "taxon"]
+
+### exrpoting directory
+dir_out = "1_raw_sequences/"
+
+for(locus_name in all_loci){
+  ### lines with information
+  access_clean = access[!is.na(access[,locus_name]),]
+  ### accession numbers and names
+  access_num = access_clean[,locus_name]
+  access_names = access_clean$species
+  ### downloading sequences
+  one_locus = read.GenBank( access.nb = access_num )
+  ### naming sequences
+  names(one_locus) = access_names
+  ### export
+  write.dna(
+    one_locus, 
+    file = paste0(dir_out, locus_name, "_raw.fas"), 
+    format = 'fasta' 
+  )
+  ### check
+  print(paste0("Download complete: ", locus_name))
+}
+
